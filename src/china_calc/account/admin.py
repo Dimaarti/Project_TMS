986f9a6bced59e8e0b2a8672e3649b916dabeeb2
dima <dima.arti333@gmail.com>
2026-07-12 20:20:54 +0300

    Begin configuration user

---
+++ src/china_calc/account/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from china_calc.account.models.users import User
+
+admin.site.register(User)
---
+++ src/china_calc/account/models/users.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+
+from china_calc.account.managers import UserManager
+from config.models import BaseModel
+
+
+class User(AbstractBaseUser, PermissionsMixin, BaseModel):
+    first_name = models.CharField(
+        max_length=64,
+        null=True,
+        blank=True,
+    )
+    last_name = models.CharField(
+        max_length=64,
+        null=True,
+        blank=True,
+    )
+    email = models.EmailField(
+        max_length=64,
+        unique=True,
+    )
+    is_staff = models.BooleanField(
+        default=False,
+    )
+
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        db_table = 'user'
+        verbose_name_plural = 'Users'
+        verbose_name = 'User'
+
+
