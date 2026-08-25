1360c54cea02721d5f9bfb745a2ac51063da31ed
dima <dima.arti333@gmail.com>
2026-08-25 21:50:19 +0300

    add home page redirect and disable debug toolbar in production

--- src/china_calc/urls.py
+++ src/china_calc/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="shipment:list", permanent=False)),
     path("account/", include("china_calc.account.urls")),
     path("shipment/", include("china_calc.shipment.urls")),
     path("client/", include("china_calc.client.urls")),
--- src/config/urls.py
+++ src/config/urls.py
@@ -19,7 +19,13 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import include, path
 
+from config import settings
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("china_calc.urls")),
-] + debug_toolbar_urls()
+]
+if settings.DEBUG:
+    from debug_toolbar.toolbar import debug_toolbar_urls
+
+    urlpatterns += debug_toolbar_urls()
