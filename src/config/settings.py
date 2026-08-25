43d7e6cd7ffd8549a445d9f3ae18cb94aae37ad9
dima <dima.arti333@gmail.com>
2026-08-25 21:42:06 +0300

    allowed hosts

--- src/config/settings.py
+++ src/config/settings.py
@@ -23,7 +23,9 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["testserver", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = env.list(
+    "ALLOWED_HOSTS", default=["testserver", "localhost", "127.0.0.1"]
+)
 
 CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=[])
 
